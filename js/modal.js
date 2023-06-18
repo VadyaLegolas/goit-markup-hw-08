4216fa7941006a67cc162438a2bb87f82a24a847
Vlad <vadya-85@mail.ru>
2023-06-18 18:58:14 +0200

    fix fonts and modal checkbox

--- js/modal.js
+++ js/modal.js
@@ -5,10 +5,16 @@
     modal: document.querySelector("[data-modal]"),
   };
 
+  const bodyLock = document.querySelector("body");
+  const htmlLock = document.querySelector("html");
+
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+
+    bodyLock.classList.toggle("lock");
+    htmlLock.classList.toggle("lock");
   }
 })();
